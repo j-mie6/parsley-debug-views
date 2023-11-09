7f41c8aabfe6d0edcb0bea1a37606439817e092c
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-11-09 18:42:57 +0000

    feat: added json string variant

--- json-info/shared/src/main/scala/parsley/debugger/frontend/JsonFormatter.scala
+++ json-info/shared/src/main/scala/parsley/debugger/frontend/JsonFormatter.scala
@@ -12,7 +12,7 @@ import ToJson.Implicits._
   * impure. It is recommended that if one wants to extract multiple inputs from the same frontend with the same
   * continuation, that it is recorded to some kind of list or iterable structure.
   */
-case class JsonFormatter(cont: ujson.Value => Unit) extends StatelessFrontend {
+class JsonFormatter(cont: ujson.Value => Unit) extends StatelessFrontend {
   override protected def processImpl(input: => String, tree: => DebugTree): Unit =
     cont(
       ujson.Obj(
@@ -20,4 +20,17 @@ case class JsonFormatter(cont: ujson.Value => Unit) extends StatelessFrontend {
         "parse" -> tree.toJson
       )
     )
+}
+
+object JsonFormatter {
+  def apply(cont: ujson.Value => Unit): JsonFormatter = new JsonFormatter(cont)
+}
+
+/** A version of [[JsonFormatter]] that takes a JSON string instead of a [[ujson.Value]] object. */
+class JsonStringFormatter(cont: String => Unit, indent: Int = 2, escapeUnicode: Boolean = false)
+  extends JsonFormatter(json => cont(json.render(indent, escapeUnicode = escapeUnicode)))
+
+object JsonStringFormatter {
+  def apply(cont: String => Unit, indent: Int = 2, escapeUnicode: Boolean = false): JsonStringFormatter =
+    new JsonStringFormatter(cont, indent, escapeUnicode)
 }
