d15b14d8dfb78b46871a106b0f2b68294fe4b7e5
rileyhorrix <riley@horrix.com>
2025-01-18 21:01:19 +0000

    [refactor]: Changes made according to PR review.

--- remote-view/shared/src/main/scala/parsley/debug/RemoteView.scala
+++ remote-view/shared/src/main/scala/parsley/debug/RemoteView.scala
@@ -9,8 +9,12 @@ import parsley.debug.internal.DebugTreeSerialiser
 
 import sttp.client3.*
 
-import scala.util.Try
+// import scala.util.Try
 import scala.concurrent.duration.*
+import sttp.model.Uri
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
 
 /** The RemoteView HTTP module allows the parsley debug tree to be passed off to a server through a specified port on
   * local host (by default) or to a specified IP address. This enables all of the debug tree parsing, serving and 
@@ -24,107 +28,96 @@ import scala.concurrent.duration.*
   * libraries like circe for its improved speed over large data structures.
   */
 sealed trait RemoteView extends DebugView.Reusable {
-  implicit protected val port: Integer
-  implicit protected val address: String
+  protected val port: Int
+  protected val address: String
 
+  // Printing helpers
+  private [debug] final val TextToRed: String    = "\u001b[31m"
+  private [debug] final val TextToNormal: String = "\u001b[0m"
+  
+  // Request Timeouts
+  private [debug] final val ConnectionTimeout: FiniteDuration = 30.second
+  private [debug] final val ResponseTimeout: FiniteDuration = 10.second
+
+  // Endpoint for post request
+  private [debug] final val endPoint: Uri = uri"http://$address:$port/api/remote"
+
+  
   /**
-    * Send the debug tree and input to the port and address specified in the 
-    * object construction. 
-    *
-    * @param input The input source.
-    * @param tree The debug tree.
-    */
+   * Send the debug tree and input to the port and address specified in the 
+   * object construction. 
+   *
+   * @param input The input source.
+   * @param tree The debug tree.
+   */
   override private [debug] def render(input: => String, tree: => DebugTree): Unit = {
-    // Printing helpers
-    val TEXT_TO_RED: String    = "\u001b[31m"
-    val TEXT_TO_NORMAL: String = "\u001b[0m"
-
-    val CONNECTION_TIMEOUT: FiniteDuration = 30.second
-    val RESPONSE_TIMEOUT: FiniteDuration = 10.second
-
-    // Create localhost endpoint using port
-    val sb: StringBuilder = new StringBuilder("http://")
-    sb.append(address)
-    sb.append(':')
-    sb.append(port.toString())
-    sb.append("/remote")
-    val endPoint: String = sb.result()
-
-    // Format payload as JSON
-    sb.clear()
-    sb.append("{\"input\": \"")
-    sb.append(input)
-    sb.append("\", \"tree\": ")
-    sb.append(DebugTreeSerialiser.toJSON(tree))
-    sb.append("}")
-    val payload: String = sb.result()
-
-    println("Sending Debug Tree to Server")
-
+    // JSON formatted payload for post request
+    val payload: String = s"{\"input\":\"$input\",\"tree\":${DebugTreeSerialiser.toJSON(tree)}}"
+    
     // Send POST
-    try {
-      val backend = HttpURLConnectionBackend(
-        options = SttpBackendOptions.connectionTimeout(CONNECTION_TIMEOUT)
-      )
-      val r: Response[Either[String, String]] = basicRequest
-        .readTimeout(RESPONSE_TIMEOUT)
-        .header("User-Agent", "remoteView")
-        .contentType("application/json")
-        .body(payload)
-        .post(uri"$endPoint")
-        .send(backend)
-        
-        if (r.body.isLeft) {
-          println(s"${TEXT_TO_RED}Request Failed with message : ${r.body.getOrElse("<no-response>")}${TEXT_TO_NORMAL}")
-        } else {
-          println(s"Response from server : ${r.body.getOrElse("<no-response>")}")
-        }
-    } catch {
-      case _: Throwable => println(s"${TEXT_TO_RED}Remote View request failed! Please validate address ($address) and port number ($port).${TEXT_TO_NORMAL}")
-    }
+    println("Sending Debug Tree to Server")
+    
+    val backend = TryHttpURLConnectionBackend(
+      options = SttpBackendOptions.connectionTimeout(ConnectionTimeout)
+    )
 
+    val response: Try[Response[Either[String,String]]] = basicRequest
+      .readTimeout(ResponseTimeout)
+      .header("User-Agent", "remoteView")
+      .contentType("application/json")
+      .body(payload)
+      .post(endPoint)
+      .send(backend)
+
+    response match {
+      case Failure(exception) => println(s"${TextToRed}Remote View request failed! Please validate address ($address) and port number ($port).${TextToNormal}\n\tError : ${exception.toString()}")
+      case Success(value) => value.body match {
+        // Left indicates the request is successful, but the response code was not 2xx.
+        case Left(errorMessage) => println(s"${TextToRed}Request Failed with message : $errorMessage, and status code : ${value.code}${TextToNormal}")
+        // Right indicates a successful request with 2xx response code.
+        case Right(body) => println(s"Request successful with message : $body")
+      }
+    }
   }
 }
 
 object RemoteView extends DebugView.Reusable with RemoteView {
     // Default port uses HTTP port and local host
-    override implicit val port = 80
-    override implicit val address = "127.0.0.1"
+    override protected val port: Int = 80
+    override protected val address: String = "127.0.0.1"
     
+    private final val MinimalIpLength: Int = "0.0.0.0".length
+    private final val MaximalIpLength: Int = "255.255.255.255".length
+    
+    private final val MaxUserPort: Integer = 0xFFFF
+
     /** Do some basic validations for a given IP address. */
     private def checkIp(address: String): Boolean = {
-      val minimalAddress: String = "0.0.0.0"
-      val maximalAddress: String = "255.255.255.255"
-
-      if (address.length() < minimalAddress.length() || address.length() > maximalAddress.length()) {
-        return false
-      }
-      
-      if (address.count(_ == '.') != 3) {
-        return false
-      }
+      val addrLenValid: Boolean = address.length < MinimalIpLength || address.length > MaximalIpLength
+      val addrDotValid: Boolean = address.count(_ == '.') != 3
       
       // Check that every number is a number
       val numberStrings: Array[String] = address.split('.')
-      numberStrings.forall((number: String) => number.length() > 0 && Try(number.toInt).toOption.nonEmpty)
+      val addrNumValid: Boolean = numberStrings.forall((number: String) => number.length > 0 && number.toIntOption.nonEmpty)
+
+      addrLenValid && addrDotValid && addrNumValid 
     }
 
     /** Create a new instance of [[RemoteView]] with a given custom port. */
-    def apply(user_port: Integer = port, user_address: String = address): RemoteView = new RemoteView {
-      private val MAX_USER_PORT: Integer = 65535
-      assert(user_port <= MAX_USER_PORT, s"Remote View port invalid : $user_port > $MAX_USER_PORT")
-      assert(checkIp(user_address), s"Remote View address invalid : $user_address")
+    def apply(userPort: Integer = port, userAddress: String = address): RemoteView = new RemoteView {
+      require(userPort <= MaxUserPort, s"Remote View port invalid : $userPort > $MaxUserPort")
+      require(checkIp(userAddress), s"Remote View address invalid : $userAddress")
 
-      override implicit val port = user_port
-      override implicit val address = user_address
+      override implicit val port = userPort
+      override implicit val address = userAddress
     }
 }
 
 /** Helper object for connecting to the DILL backend. */
-object DillRemoteView extends RemoteView {
+object DillRemoteView extends DebugView.Reusable with RemoteView {
   // Default endpoint for DILL backend is port 5173 on localhost
-  override implicit protected val address: String = "127.0.0.1"
-  override implicit protected val port: Integer = 5173
+  override protected val port: Int = 5173
+  override protected val address: String = "127.0.0.1"
 
   /** Create a new instance of [[RemoteView]] with default ports for the DILL backend server. */
   def apply(): RemoteView = RemoteView(port, address)
